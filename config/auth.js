5dea649da32a703d96035c3a71d4b73951f426c4
vatsals9 <vatsal.sharma1@gmail.com>
2016-08-10 11:23:44 +0530

    Added files from pc to git to keep note of changes for the first time

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    
+    'googleAuth' : {
+        'clientID'      : '629880394673-3ji0bfjd5mebjrjv08oe65qor0rq4iqj.apps.googleusercontent.com',
+        'clientSecret'  : '2rZp3vgEnrvhwSgUsiWpw0sQ',
+        'callbackURL'   : 'http://localhost:8090/auth/google/callback'
+    }
+
+};
